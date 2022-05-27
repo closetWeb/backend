d4c3be4cb79823f12012ffb1069779ec5d09630f
정인교 <69563418+ingyocode@users.noreply.github.com>
2022-05-27 23:52:17 +0900

    - chore: 불필요한 코드 삭제
    test코드나 기본 코드.

--- closet-server/apps/closet-server/src/app/app.module.ts
+++ closet-server/apps/closet-server/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthControllerModule } from './domains/auth/auth.controller.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersEntity } from '@closet-web/models';
+import { GlobalTypeOrmModule } from '@closet-web/globals';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthControllerModule, TypeOrmModule.forFeature([UsersEntity]), GlobalTypeOrmModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
