24234d8d75819c8f0a184cfb7c60b303ba27c03f
정인교 <69563418+ingyocode@users.noreply.github.com>
2022-06-09 19:12:05 +0900

    - docs(root) : add swaggerModule

--- closet-server/apps/closet-server/src/main.ts
+++ closet-server/apps/closet-server/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
@@ -12,11 +13,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const options = new DocumentBuilder()
+    .setTitle('PLASK API V3')
+    .setDescription('closet web swagger')
+    .setVersion('3.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableCors();
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
